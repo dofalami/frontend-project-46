3ec2fa66fbd7f224d7275e9d7a611424d0a36ab6
dofalami <polina.chugaeva@gmail.com>
2023-04-02 17:04:16 +0300

    upd

---
+++ bin/gendiff.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const { Command } = require('commander');
+const program = new Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('-V, --version', 'output the version number')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2, options) => {
+    console.log(genDiff(filepath1, filepath2, options.format));
+  });
+
+program.parse(process.argv);
