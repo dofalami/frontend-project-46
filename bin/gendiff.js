d6424d97f1a8fdd77ef5d5c783db9ef5b20ea22f
dofalami <polina.chugaeva@gmail.com>
2023-04-02 18:06:32 +0300

    upd

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
-//  .option('-f, --format <type>', 'output format', 'stylish')
+  .option('-f, --format <type>', 'output format')
 //  .action((filepath1, filepath2, options) => {
 //    console.log(genDiff(filepath1, filepath2, options.format));
 //  });
